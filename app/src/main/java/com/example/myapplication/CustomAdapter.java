d9b42655bf15d084f47f425b91f76778349adf3c
AbshirMohammed <mohammedabshir@gmail.com>
2019-12-19 14:14:38 +0300

    yah yeet

--- app/src/main/java/com/example/myapplication/CustomAdapter.java
+++ app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -1,40 +1,72 @@
-/*package com.example.myapplication;
+package com.example.myapplication;
 
 import android.content.Context;
+//import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CustomAdapter extends BaseAdapter {
+import androidx.annotation.NonNull;
+
+import java.io.File;
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter<String> {
     private Context mContext;
-    private String[]  Title;
-    private int[] imge;
+    private List<String>  mValues;
+    private int[] mIcons;
 
-    public CustomAdapter(Context context, String[] text1,int[] imageIds) {
-        mContext = context;
-        Title = text1;
-        imge = imageIds;
-
+    public CustomAdapter(Context context, List<String> values, int[] icons) {
+        super(context, R.layout.row);
+        this.mValues = values;
+        this.mIcons = icons;
+        this.mContext = context;
     }
 
     @Override
     public int getCount() {
-        return values.size();
+        return mValues.size();
     }
 
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
+    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder mViewHolder = new ViewHolder();
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater) mContext.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.row, parent, false);
+            mViewHolder.mFlag = (ImageView) convertView.findViewById(R.id.imageView);
+            mViewHolder.mName = (TextView) convertView.findViewById(R.id.textView);
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        mViewHolder.mName.setText(mValues.get(position));
+
+        String a = mValues.get(position);
+
+        File p = mContext.getExternalFilesDir(a);
+        //File f = new File(path);
+
+        if (p.isDirectory()) {
+            mViewHolder.mFlag.setImageResource(mIcons[0]);
+        }else if(p.isFile()) {
+            mViewHolder.mFlag.setImageResource(mIcons[1]);
+        }
+        else{
+            mViewHolder.mFlag.setImageResource(mIcons[2]);
+        }
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        ImageView mFlag;
+        TextView mName;
     }
 }
-*/
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.myapplication;
 
+import android.app.AlarmManager;
 import android.app.ListActivity;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.view.ContextMenu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends ListActivity {
 
     private String path;
-    //private ListView listView1;
+    int[] icons = {R.drawable.folder,R.drawable.file,R.drawable.help2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_files);
+
+        ListView listView = findViewById(android.R.id.list);
+        registerForContextMenu(listView);
 
         // Use the current directory as title
 
@@ -35,11 +50,19 @@ public class MainActivity extends ListActivity {
         }
         pathOutput.setText("Storage"+path.substring(19));
 
+        final ImageButton b1 = findViewById(R.id.imageButton);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"Welcome, this app automatically deletes files from chosen folders at the end of the month. Long press folders to activate, long press files to exempt them from deletion.", Toast.LENGTH_LONG).show();
+            }
+        });
+
         // Read all files sorted into the values-array
-        List values = new ArrayList();
+        List<String> values = new ArrayList();
         File dir = new File(path);
         if (!dir.canRead()) {
-            setTitle(getTitle() + " (inaccessible)");
+            pathOutput.setText("(inaccessible)");
         }
         String[] list = dir.list();
         if (list != null) {
@@ -48,19 +71,45 @@ public class MainActivity extends ListActivity {
                     values.add(file);
                 }
             }
+            if (values.isEmpty()){
+                Toast.makeText(this,"This directory is empty.", Toast.LENGTH_LONG).show();
+            }
         }
         Collections.sort(values);
 
-      /*  CustomAdapter adapter = new CustomAdapter(this,  textString, drawableIds);
 
-
-        listView1 = (ListView)findViewById(R.id.list);
-        listView1.setAdapter(adapter);
-*/
         // Put the data into the list
-        ArrayAdapter adapter = new ArrayAdapter(this,
+       ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_2, android.R.id.text1, values);
         setListAdapter(adapter);
+
+        CustomAdapter myAdapter = new CustomAdapter(MainActivity.this, values, icons);
+        listView.setAdapter(myAdapter);
+
+
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean("firstTime", false)) {
+
+            Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+            AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.DAY_OF_MONTH, 19);
+            //calendar.set(Calendar.HOUR_OF_DAY, 7);
+            //calendar.set(Calendar.MINUTE, 0);
+            //calendar.set(Calendar.SECOND, 1);
+
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, pendingIntent);
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            editor.apply();
+        }
     }
 
     @Override
@@ -79,5 +128,54 @@ public class MainActivity extends ListActivity {
             Toast.makeText(this, filename + " is not a directory", Toast.LENGTH_LONG).show();
         }
     }
-}
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        int index = info.position;
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        String filename = (String) getListAdapter().getItem(index);
+
+        if (path.endsWith(File.separator)) {
+            filename = path + filename;
+        } else {
+            filename = path + File.separator + filename;
+        }
+
+
+
+        if (new File(filename).isDirectory()) {
+            menu.setHeaderTitle("Choose option for this folder:");
+            menu.add(0, v.getId(), 0, "Set for deletion.");
+        } else {
+            menu.setHeaderTitle("Choose option for this file:");
+            menu.add(0, v.getId(), 0, "Exempt file.");
+        }
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int index = info.position;
+
+        String filename = (String) getListAdapter().getItem(index);
+        if (path.endsWith(File.separator)) {
+            filename = path + filename;
+        } else {
+            filename = path + File.separator + filename;
+        }
+
+        if (new File(filename).isDirectory()) {
+            Toast.makeText(this, "The folder at " + filename + " will be cleared at the end of each month.", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "The file at " + filename + " is exempt from deletion.", Toast.LENGTH_LONG).show();
+        }
+
+            return true;
+    }
+}
